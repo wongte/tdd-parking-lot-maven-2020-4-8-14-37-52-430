b40f075d2531fe5747561a9a085201e4c6631829
Terrence Wong <wongte@oocl.com>
2020-04-09 15:38:14 +0800

    [Terrence] - add default capacity in ParkingLot

--- src/main/java/com/oocl/ParkingLot.java
+++ src/main/java/com/oocl/ParkingLot.java
@@ -1,10 +1,15 @@
 package com.oocl;
 
 public class ParkingLot {
+    private final int DEFAULT_CAPACITY = 10;
     private final int capacity;
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+    }
+
+    public ParkingLot() {
+        this.capacity = DEFAULT_CAPACITY;
     }
 
     public int getCapacity() {
